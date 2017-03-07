e2cd6990e9ebd33de65d36f2c576836798f20338
Sumankumar Panchal <suman@ece.iisc.ernet.in>
2017-03-07 18:02:50 +0530

    Added TSCH Border router and cooja example.

--- examples/ipv6/rpl-tsch/node.c
+++ examples/ipv6/rpl-tsch/node.c
@@ -41,7 +41,6 @@
 #include "net/rpl/rpl.h"
 #include "net/ipv6/uip-ds6-route.h"
 #include "net/mac/tsch/tsch.h"
-#include "net/rpl/rpl-private.h"
 #if WITH_ORCHESTRA
 #include "orchestra.h"
 #endif /* WITH_ORCHESTRA */
@@ -69,76 +68,46 @@ print_network_status(void)
   int i;
   uint8_t state;
   uip_ds6_defrt_t *default_route;
-#if RPL_WITH_STORING
   uip_ds6_route_t *route;
-#endif /* RPL_WITH_STORING */
-#if RPL_WITH_NON_STORING
-  rpl_ns_node_t *link;
-#endif /* RPL_WITH_NON_STORING */
 
-  PRINTF("--- Network status ---\n");
-
+  PRINTA("--- Network status ---\n");
+  
   /* Our IPv6 addresses */
-  PRINTF("- Server IPv6 addresses:\n");
+  PRINTA("- Server IPv6 addresses:\n");
   for(i = 0; i < UIP_DS6_ADDR_NB; i++) {
     state = uip_ds6_if.addr_list[i].state;
     if(uip_ds6_if.addr_list[i].isused &&
        (state == ADDR_TENTATIVE || state == ADDR_PREFERRED)) {
-      PRINTF("-- ");
-      PRINT6ADDR(&uip_ds6_if.addr_list[i].ipaddr);
-      PRINTF("\n");
+      PRINTA("-- ");
+      uip_debug_ipaddr_print(&uip_ds6_if.addr_list[i].ipaddr);
+      PRINTA("\n");
     }
   }
-
+  
   /* Our default route */
-  PRINTF("- Default route:\n");
+  PRINTA("- Default route:\n");
   default_route = uip_ds6_defrt_lookup(uip_ds6_defrt_choose());
   if(default_route != NULL) {
-    PRINTF("-- ");
-    PRINT6ADDR(&default_route->ipaddr);
-    PRINTF(" (lifetime: %lu seconds)\n", (unsigned long)default_route->lifetime.interval);
+    PRINTA("-- ");
+    uip_debug_ipaddr_print(&default_route->ipaddr);;
+    PRINTA(" (lifetime: %lu seconds)\n", (unsigned long)default_route->lifetime.interval);
   } else {
-    PRINTF("-- None\n");
+    PRINTA("-- None\n");
   }
 
-#if RPL_WITH_STORING
   /* Our routing entries */
-  PRINTF("- Routing entries (%u in total):\n", uip_ds6_route_num_routes());
+  PRINTA("- Routing entries (%u in total):\n", uip_ds6_route_num_routes());
   route = uip_ds6_route_head();
   while(route != NULL) {
-    PRINTF("-- ");
-    PRINT6ADDR(&route->ipaddr);
-    PRINTF(" via ");
-    PRINT6ADDR(uip_ds6_route_nexthop(route));
-    PRINTF(" (lifetime: %lu seconds)\n", (unsigned long)route->state.lifetime);
-    route = uip_ds6_route_next(route);
+    PRINTA("-- ");
+    uip_debug_ipaddr_print(&route->ipaddr);
+    PRINTA(" via ");
+    uip_debug_ipaddr_print(uip_ds6_route_nexthop(route));
+    PRINTA(" (lifetime: %lu seconds)\n", (unsigned long)route->state.lifetime);
+    route = uip_ds6_route_next(route); 
   }
-#endif
-
-#if RPL_WITH_NON_STORING
-  /* Our routing links */
-  PRINTF("- Routing links (%u in total):\n", rpl_ns_num_nodes());
-  link = rpl_ns_node_head();
-  while(link != NULL) {
-    uip_ipaddr_t child_ipaddr;
-    uip_ipaddr_t parent_ipaddr;
-    rpl_ns_get_node_global_addr(&child_ipaddr, link);
-    rpl_ns_get_node_global_addr(&parent_ipaddr, link->parent);
-    PRINTF("-- ");
-    PRINT6ADDR(&child_ipaddr);
-    if(link->parent == NULL) {
-      memset(&parent_ipaddr, 0, sizeof(parent_ipaddr));
-      PRINTF(" --- DODAG root ");
-    } else {
-      PRINTF(" to ");
-      PRINT6ADDR(&parent_ipaddr);
-    }
-    PRINTF(" (lifetime: %lu seconds)\n", (unsigned long)link->lifetime);
-    link = rpl_ns_node_next(link);
-  }
-#endif
-
-  PRINTF("----------------------\n");
+  
+  PRINTA("----------------------\n");
 }
 /*---------------------------------------------------------------------------*/
 static void
@@ -165,29 +134,16 @@ PROCESS_THREAD(node_process, ev, data)
   PROCESS_BEGIN();
 
   /* 3 possible roles:
-   * - role_6ln: simple node, will join any network, secured or not
-   * - role_6dr: DAG root, will advertise (unsecured) beacons
-   * - role_6dr_sec: DAG root, will advertise secured beacons
-   * */
+     * - role_6ln: simple node, will join any network, secured or not
+     * - role_6dr: DAG root, will advertise (unsecured) beacons
+     * - role_6dr_sec: DAG root, will advertise secured beacons
+     * */
   static int is_coordinator = 0;
   static enum { role_6ln, role_6dr, role_6dr_sec } node_role;
   node_role = role_6ln;
-
-  int coordinator_candidate = 0;
-
-#ifdef CONTIKI_TARGET_Z1
-  /* Set node with MAC address c1:0c:00:00:00:00:01 as coordinator,
-   * convenient in cooja for regression tests using z1 nodes
-   * */
-  extern unsigned char node_mac[8];
-  unsigned char coordinator_mac[8] = { 0xc1, 0x0c, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01 };
-
-  coordinator_candidate = (memcmp(node_mac, coordinator_mac, 8) == 0);
-#elif CONTIKI_TARGET_COOJA
-  coordinator_candidate = (node_id == 1);
-#endif
-
-  if(coordinator_candidate) {
+  
+  /* Set node with ID == 1 as coordinator, convenient in Cooja. */
+  if(node_id == 1) {
     if(LLSEC802154_ENABLED) {
       node_role = role_6dr_sec;
     } else {
@@ -206,8 +162,8 @@ PROCESS_THREAD(node_process, ev, data)
 
     while(!etimer_expired(&et)) {
       printf("Init: current role: %s. Will start in %u seconds. Press user button to toggle mode.\n",
-             node_role == role_6ln ? "6ln" : (node_role == role_6dr) ? "6dr" : "6dr-sec",
-             CONFIG_WAIT_TIME);
+                node_role == role_6ln ? "6ln" : (node_role == role_6dr) ? "6dr" : "6dr-sec",
+                CONFIG_WAIT_TIME);
       PROCESS_WAIT_EVENT_UNTIL(((ev == sensors_event) &&
                                 (data == &button_sensor) && button_sensor.value(0) > 0)
                                || etimer_expired(&et));
@@ -224,31 +180,31 @@ PROCESS_THREAD(node_process, ev, data)
 #endif /* CONFIG_VIA_BUTTON */
 
   printf("Init: node starting with role %s\n",
-         node_role == role_6ln ? "6ln" : (node_role == role_6dr) ? "6dr" : "6dr-sec");
+      node_role == role_6ln ? "6ln" : (node_role == role_6dr) ? "6dr" : "6dr-sec");
 
   tsch_set_pan_secured(LLSEC802154_ENABLED && (node_role == role_6dr_sec));
   is_coordinator = node_role > role_6ln;
 
   if(is_coordinator) {
     uip_ipaddr_t prefix;
-    uip_ip6addr(&prefix, UIP_DS6_DEFAULT_PREFIX, 0, 0, 0, 0, 0, 0, 0);
+    uip_ip6addr(&prefix, 0xaaaa, 0, 0, 0, 0, 0, 0, 0);
     net_init(&prefix);
   } else {
     net_init(NULL);
   }
-
+  
 #if WITH_ORCHESTRA
   orchestra_init();
 #endif /* WITH_ORCHESTRA */
-
+  
   /* Print out routing tables every minute */
   etimer_set(&et, CLOCK_SECOND * 60);
-  while(1) {
+  while(1) {      
     print_network_status();
     PROCESS_YIELD_UNTIL(etimer_expired(&et));
     etimer_reset(&et);
   }
-
+  
   PROCESS_END();
 }
 /*---------------------------------------------------------------------------*/
